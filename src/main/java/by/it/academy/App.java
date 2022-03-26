71d4dd099ab4da919436578d80f8e38167e19a83
geron-d <83971294+geron-d@users.noreply.github.com>
2022-03-26 11:55:23 +0300

    Artem has been added new string

--- src/main/java/by/it/academy/App.java
+++ src/main/java/by/it/academy/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! I am Anton" );
+        System.out.println( "Hello World! I am Anton. Artem is here too." );
     }
 }
